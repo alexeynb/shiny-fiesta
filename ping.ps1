4d80e611490fa8fbb38d7eb8a33ec1b0886794d2
alexeynb <alexeynb@inbox.ru>
2025-10-15 10:27:58 +0300

    Update ping.ps1

--- ping.ps1
+++ ping.ps1
@@ -1,9 +1,7 @@
 $range = 1..255 | ForEach-Object { "192.168.0.$_" }
 
-foreach ($ip in $range) {
-    Write-Host "Пинг $ip" -ForegroundColor Cyan
-    Test-Connection -ComputerName $ip -Count 1 -Quiet -ErrorAction SilentlyContinue | Out-Null
+$range | ForEach-Object -Parallel {
+    Test-Connection -ComputerName $_ -Count 1 -Quiet -ErrorAction SilentlyContinue | Out-Null
 }
 
-Write-Host "`nARP-таблица обновлена:`n" -ForegroundColor Green
 arp -a
